0a26b70782e45b252156e5a180e5b92a3fd876ad
lear <1325925707@qq.com>
2019-06-24 13:28:28 +0800

    20190624: 每日一A

---
+++ AC自动机/189.旋转数组.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=javascript
+ *
+ * [189] 旋转数组
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+  nums.unshift(...nums.splice(nums.length - k, k));
+};
+

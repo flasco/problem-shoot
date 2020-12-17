4b5ff1d26be376d3320ea52954d78d17f55722f8
czq <chenzhengqiang@youzan.com>
2020-12-17 16:38:17 +0800

    20201217: 有空一A

---
+++ AC自动机/746.使用最小花费爬楼梯.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=javascript
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+  cost.push(0);
+  const dp = [cost[0], cost[1]];
+  for (let i = 2; i < cost.length; i++) {
+    // dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+    dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+  }
+  return dp.pop();
+};
+// @lc code=end

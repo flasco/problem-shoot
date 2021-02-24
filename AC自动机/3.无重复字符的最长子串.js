03681185a760f062c09aabd8a1c026446eab4346
flasco <flasco@163.com>
2021-02-24 19:41:40 +0800

    fix: 补充一点注释

--- AC自动机/3.无重复字符的最长子串.js
+++ AC自动机/3.无重复字符的最长子串.js
@@ -15,13 +15,12 @@ var lengthOfLongestSubstring = function (s) {
   let q = 0;
 
   const len = s.length;
+  // 双指针滑动窗口，如果遇到重复的字符，就把左指针移动到老的重复的 index + 1 的位置
+  // 可能是 string 直接 substring 的性能有点差，不过差不了太多，问题不大
   while (q < len) {
     const curStr = s.substring(p, q);
-    const index = curStr.indexOf(s[q]);
-    if (index > -1) {
-      p += index + 1;
-    }
-    q++;
+    const index = curStr.indexOf(s[q++]);
+    if (index > -1) p += index + 1;
     max = Math.max(max, q - p);
   }
 

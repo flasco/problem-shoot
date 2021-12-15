a618d2398564bbcef367c0f500fe51e9f2a7fd69
flasco <flasco@163.com>
2021-12-15 23:01:23 +0800

    fix: 修改

--- index.js
+++ index.js
@@ -1,44 +1,77 @@
-const section = document.createElement('section');
-
-section.innerHTML = `
-<!doctype html>
-<html>
+const $section = document.createElement("section");
+$section.innerHTML = `
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <script async src="https://cdnjs.cloudflare.com/ajax/libs/react/17.0.1/umd/react.production.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.1/umd/react-dom.production.min.js"></script>
-    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
   </head>
   <body>
-    <div id="root"></div>
+    <div id="rootxxx"></div>
   </body>
+  <script type="text/babel" name="qwe">
+  const { useState, useRef } = React;
+
+  const App = () => {
+    const [href, setHref] = useState('');
+    const downloadingRef = useRef(false);
+    const onClick = () => {
+      if (!href) {
+        alert('请输入链接');
+        return;
+      }
+      if (!href.toLowerCase().includes('m3u8')) {
+        alert('链接有误，请重新输入');
+        return;
+      }
+
+      if (downloadingRef.current) {
+        alert('下载中');
+        return;
+      }
+      downloadingRef.current = true;
+    }
+
+    return (
+      <div style={{ padding: '20px 16px' }}>
+        <input type="text" value={href} onChange={e => setHref(e.target.value)} accept=".ttf" placeholder="请输入 m3u8 链接" />
+        <button onClick={onClick}>解析m3u8</button>
+      </div>
+    );
+  };
+  ReactDOM.render(React.createElement(App), document.getElementById('rootxxx'));
+  </script>
 </html>
 `;
 
-document.body.appendChild(section);
+$section.style.width = "100%";
+$section.style.height = "800px";
+$section.style.top = "0";
+$section.style.left = "0";
+$section.style.position = "relative";
+$section.style.zIndex = "9999";
+$section.style.backgroundColor = "white";
+document.body.appendChild($section);
 
-const loadScript = (str, callback = () => {}) => {
-  const scripts = document.createElement('script');
-  scripts.src = str;
-  scripts.addEventListener('load', callback);
-  document.body.appendChild(scripts);
-};
+function __loadScript(url) {
+  return new Promise((res) => {
+    const s = document.createElement("script");
+    s.src = url;
+    s.addEventListener("load", () => res(s));
+    document.body.appendChild(s);
+  });
+}
 
-loadScript(
-  'https://cdnjs.cloudflare.com/ajax/libs/react/17.0.1/umd/react.production.min.js'
-);
-loadScript(
-  'https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.1/umd/react-dom.production.min.js'
-);
-loadScript('https://unpkg.com/@babel/standalone/babel.min.js');
-
-const scripts = document.createElement('script');
-scripts.type = 'text/label';
-scripts.innerHTML = () => {
-  const Tester = () => {
-    return `<div>123</div>`
-  };
-  ReactDOM.render(React.createElement(Tester, section.querySelector('#root')))
-};
-document.body.appendChild(scripts);
-
-alert('注入成功，请滚动到页面底部');
+Promise.all([
+  __loadScript("https://unpkg.com/react@17/umd/react.production.min.js"),
+  __loadScript("https://unpkg.com/@babel/standalone@7.13.15/babel.min.js"),
+])
+.then(() => __loadScript("https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"))
+.then(() => {
+  const scripts = document.querySelector('script[name="qwe"]');
+  const { code } = Babel.transform(scripts.innerText, { presets: ["react"] });
+  eval(code);
+  alert("inject ok, scroll end to get it");
+});
